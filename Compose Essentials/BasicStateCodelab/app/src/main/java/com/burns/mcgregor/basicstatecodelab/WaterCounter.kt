b4633ec2f14b3cda8e912539862133aa088ab592
Andrew McGregor <burnsmcgregor@outlook.com>
2022-09-23 16:52:45 +0100

    Basic State Codelab - Ex9

--- Compose Essentials/BasicStateCodelab/app/src/main/java/com/burns/mcgregor/basicstatecodelab/WaterCounter.kt
+++ Compose Essentials/BasicStateCodelab/app/src/main/java/com/burns/mcgregor/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.burns.mcgregor.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -15,9 +16,9 @@ fun WaterCounter(modifier: Modifier){
         if (count > 0){
             Text(text = "You've had ${count} glasses.")
         }
-        Button(onClick = { count++ },
-            enabled = count < 10, modifier = Modifier.padding(top = 8.dp)) {
-            Text(text = "Add One")
-        }
+            Button(onClick = { count++ }, Modifier.padding(top = 8.dp),
+                enabled = count < 10) {
+                Text(text = "Add One")
+            }
     }
 }
