c38648c4bb7834bb8193cad0efdf6b74a215e8f5
Andrew McGregor <burnsmcgregor@outlook.com>
2023-06-04 13:51:46 +0100

    JCAD - Compose Basics  Completed

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,8 +14,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,6 +32,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -102,9 +109,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                         .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                     Text(text = "Hello,")
                     Text(text = name)
+                    if(expanded){
+                        Text(text = "Composem ipsum color sit lazy, padding theme elit aed do bouncy".repeat(4))
+                    }
                 }
-                ElevatedButton(onClick = { expanded = !expanded }) {
-                    Text(text = if (expanded)"Show less" else "Show more")
+                IconButton(onClick = {expanded = !expanded }) {
+
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = if (expanded) stringResource(id = R.string.show_less)
+                        else stringResource(id = R.string.Show_more)
+                    )
                 }
             }
         }
@@ -115,7 +130,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun MyAppPreview(){
     MyApp()
 }
-
+@Preview(showBackground = true, widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES, name = "dark")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
