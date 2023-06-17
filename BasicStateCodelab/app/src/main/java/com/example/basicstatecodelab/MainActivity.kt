683bb28e65494ff734114d60175b072b4fb8dd6a
Andrew McGregor <burnsmcgregor@outlook.com>
2023-06-17 14:11:54 +0100

    JCAD - BasicStateCodelab Ex 9

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.basicstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,8 +20,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,32 +48,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-Column() {
-    var count by remember{ mutableStateOf(0)}
-    if (count > 0) {
-        Text(text = "You've had ${count} glasses.", modifier = Modifier.padding(16.dp))
-    }
-    Button(onClick = {count++}, Modifier.padding(top = 8.dp),
-        enabled = count < 10) {
-        Text(text = "Add one")
+fun StatelessCounter(modifier: Modifier = Modifier, count:Int, onIncrement:() -> Unit) {
+    Column() {
+        if (count > 0) {
+            Text(text = "You've had ${count} glasses.", modifier = Modifier.padding(16.dp))
+        }
+        Button(
+            onClick = onIncrement,Modifier.padding(top = 8.dp), enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
     }
 }
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable{ mutableStateOf(0)}
+    StatelessCounter(modifier,count,{count++})
 }
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = taskName, modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = onClose) {
+            Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
 }
 
 @Preview(showBackground = true, name = "WaterCounter Preview")
 @Composable
 fun WaterCounterPreview() {
-    WaterCounter()
+    StatefulCounter(Modifier)
 }
 
-@Preview(showBackground = true, name = "Wellness Screen Preview")
+@Preview(showBackground = true, name = "Wellness Task Item Preview")
+@Composable
+fun WellnessTaskItemPreview() {
+    BasicStateCodelabTheme {
+        WellnessTaskItem(taskName = "Task 1", onClose = { })
+    }
+}
+
+@Preview(
+    showBackground = true, showSystemUi = true, name = "Wellness Screen Preview",
+    device = Devices.PIXEL_4_XL
+)
 @Composable
 fun WellnessScreenPreview() {
-    WellnessScreen()
+    BasicStateCodelabTheme() {
+        WellnessScreen()
+    }
 }
