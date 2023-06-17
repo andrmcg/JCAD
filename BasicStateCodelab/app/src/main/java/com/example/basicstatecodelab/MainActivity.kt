4b26db9be421b352d20d0b3883ea1d53510d2914
Andrew McGregor <burnsmcgregor@outlook.com>
2023-06-17 15:05:49 +0100

    JCAD - BasicStateCodelab Ex 11

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -69,21 +70,38 @@ fun StatefulCounter(modifier: Modifier = Modifier){
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter(modifier)
+        WellnessTasksList()
+    }
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(taskName: String,
+                     checked:Boolean,
+                     onCheckedChange:(Boolean) -> Unit,
+                     onClose: () -> Unit,
+                     modifier: Modifier = Modifier) {
+    var checkedState by remember{ mutableStateOf(false)}
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         Text(
             text = taskName, modifier = Modifier
                 .weight(1f)
                 .padding(start = 16.dp)
         )
+        Checkbox(checked = checked, onCheckedChange = onCheckedChange)
         IconButton(onClick = onClose) {
             Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItem(taskName:String, modifier: Modifier = Modifier){
+    var checkedState by rememberSaveable{ mutableStateOf(false)}
+    WellnessTaskItem(taskName = taskName, checked = checkedState,
+        onCheckedChange = {newValue -> checkedState = newValue}
+        , onClose = { }, modifier = modifier)
 }
 
 @Preview(showBackground = true, name = "WaterCounter Preview")
@@ -96,7 +114,7 @@ fun WaterCounterPreview() {
 @Composable
 fun WellnessTaskItemPreview() {
     BasicStateCodelabTheme {
-        WellnessTaskItem(taskName = "Task 1", onClose = { })
+        WellnessTaskItem(taskName = "Task 1",true, onClose = { }, onCheckedChange = {})
     }
 }
 
