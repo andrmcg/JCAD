431d5de799e2cd775c5171c4cdb4140cb1703858
Andrew McGregor <burnsmcgregor@outlook.com>
2023-07-06 14:45:31 +0100

    JCAD - Navigation Lesson 09

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 
 /**
  * Contract for information needed on every Rally navigation destination
@@ -59,6 +60,7 @@ object SingleAccount : RallyDestination {
     const val accountTypeArg = "account_type"
     val routeWithArgs = "${route}/{${accountTypeArg}}"
     val arguments = listOf(navArgument(accountTypeArg){type = NavType.StringType})
+    val deepLinks = listOf(navDeepLink { uriPattern = "rally://$route/{$accountTypeArg}" })
 }
 
 // Screens to be displayed in the top RallyTabRow
