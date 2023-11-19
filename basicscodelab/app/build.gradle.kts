d5230b1dd831003549f11616bfe52ded1a7f692f
Andrew McGregor <burnsmcgregor@outlook.com>
2023-11-19 14:16:09 +0000

    Basics ex13

--- basicscodelab/app/build.gradle.kts
+++ basicscodelab/app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -54,7 +54,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- basicscodelab/app/src/main/java/com/example/basicscodelab/ui/theme/Color.kt
+++ basicscodelab/app/src/main/java/com/example/basicscodelab/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Navy = Color(0xff073042)
+val Blue = Color(0xff4285f4)
+val LightBlue = Color(0xffd7effe)
+val Chartreuse = Color(0xffeff7cf)
