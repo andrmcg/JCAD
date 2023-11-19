c385c5a5a272ae97ebd01f24fbd61ff62e955e5d
Andrew McGregor <burnsmcgregor@outlook.com>
2023-11-19 16:33:31 +0000

    Basics Completed

--- basicscodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ basicscodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -29,7 +30,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
@@ -83,31 +92,43 @@ fun Greetings(modifier: Modifier, names:List<String> = List(1000){"$it"}){
 
 @Composable
 fun Greeting(name: String) {
+    Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        CardContent(name = name)
+    }
+}
 
+@Composable
+fun CardContent(name:String){
     var expanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(if (expanded)48.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow),
-        label = "Extra Padding")
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold
-                ))
-            }
-            ElevatedButton(onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show Less" else "Show More")
+    Row (modifier = Modifier
+        .padding(12.dp)
+        .animateContentSize(
+            animationSpec =
+            spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )){
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello")
+            Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold
+            ))
+            if (expanded){
+                Text(text = ("Composem ipsum color sit lazy, padding theme elit, sed do bouncy. ").repeat(4))
             }
         }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded){
+                    stringResource(id = R.string.show_less)}else {
+                    stringResource(id = R.string.show_more)})
+        }
     }
-
 }
 
 @Preview(showBackground = true)
