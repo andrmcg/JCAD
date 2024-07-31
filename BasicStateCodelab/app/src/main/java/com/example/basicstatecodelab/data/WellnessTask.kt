b9c451e6f60c99f6da6930f418ff28f48534611c
Andrew McGregor <burnsmcgregor@outlook.com>
2024-07-31 14:20:17 +0100

    Compose Essentials Complete

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/data/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/data/WellnessTask.kt
@@ -1,4 +1,10 @@
 package com.example.basicstatecodelab.data
 
-data class WellnessTask(val id:Int, val label:String, var checked:Boolean = false)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class WellnessTask(val id:Int, val label:String, var initialChecked:Boolean = false){
+    var checked by mutableStateOf(initialChecked)
+}
 
