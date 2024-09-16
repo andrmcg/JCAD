7ad1f2ee0f34a1af108a9bf9b30aa2f203851869
Andrew McGregor <burnsmcgregor@outlook.com>
2024-09-16 14:23:52 +0100

    JCAD - Basic State Codelab Ex09

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/helpers/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/helpers/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.basicstatecodelab.helpers
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -9,30 +10,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
+fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableIntStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
-            var showTask by remember {
-                mutableStateOf(true)
-            }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = {showTask = false })
-            }
-                Text(
-                    text = "You've had $count glasses."
-                )
+            Text(
+                text = "You've had $count glasses."
+            )
         }
-        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp), enabled = count < 10) {
+        Button(
+            onClick = { count++ }, enabled = count < 10
+        ) {
             Text(text = "Add one")
         }
+
     }
 
 }
