f5680c8fb9835ae2a73a62227bd53419e257627f
Andrew McGregor <burnsmcgregor@outlook.com>
2024-09-22 15:01:03 +0100

    JCAD - Basic State Codelab Ex10 - restore item state

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/helpers/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/helpers/WaterCounter.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+/*
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
@@ -32,4 +33,28 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 
     }
 
+}*/
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses."
+            )
+        }
+            Button(
+                onClick = onIncrement, Modifier.padding(top = 8.dp),
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
 }
--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screens/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/screens/WellnessScreen.kt
@@ -1,10 +1,16 @@
 package com.example.basicstatecodelab.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.basicstatecodelab.helpers.WaterCounter
+import com.example.basicstatecodelab.data.WellnessTasksList
+import com.example.basicstatecodelab.helpers.StatefulCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier){
-    WaterCounter(modifier)
+    //WaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter(modifier)
+        WellnessTasksList()
+    }
 }
